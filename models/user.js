2a2909904136284fa7d06b7708d463eb6d216569
Edicemi <victoriataiwo1998@gmail.com>
2022-02-08 21:29:53 +0100

    changes to users model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const mgtSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        reqired: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    authyId: String,
+    role: {
+        type: String,
+        enum: ["Manager", "tenantAdmin", "User"],
+        required: true,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('roles', mgtSchema);
